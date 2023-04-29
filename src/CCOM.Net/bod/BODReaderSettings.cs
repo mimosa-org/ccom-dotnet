98816922fec602a99336f157a151330d3b0a85a5
Matt Selway <30459857+mattys101@users.noreply.github.com>
2023-04-29 23:54:06 +0930

    Added XSDs to out dir and helper method to path

--- src/CCOM.Net/bod/BODReaderSettings.cs
+++ src/CCOM.Net/bod/BODReaderSettings.cs
@@ -2,5 +2,13 @@ namespace CommonBOD;
 
 public class BODReaderSettings
 {
-    public string SchemaPath { get; set; } = "";
+    /// <summary>
+    /// The 'XSD' folder at the location of the binaries or the working directory.
+    /// </summary>
+    public static readonly string DefaultSchemaPath = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()?.Location) ?? "XSD";
+
+    /// <summary>
+    /// Path to the folder containing the CCOM and OAGIS XML schemas.
+    /// </summary>
+    public string SchemaPath { get; set; } = DefaultSchemaPath;
 }
