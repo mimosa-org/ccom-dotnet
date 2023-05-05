b44a5f5f3e19b86e076cd3ae39c4a99893b623c4
Matt Selway <30459857+mattys101@users.noreply.github.com>
2023-05-05 19:50:38 +0930

    Fixed filenames for GeneridBodType and data area
    Somehow the filenames got switched without it being noticed.

--- src/CCOM.Net/bod/GenericBodType.cs
+++ src/CCOM.Net/bod/GenericBodType.cs
@@ -1,18 +1,93 @@
 using Oagis;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace CommonBOD;
 
 [Serializable]
-public class GenericDataAreaType<TVerb, TNoun>
+public class GenericBodType<TVerb, TNoun> : BusinessObjectDocumentType
     where TVerb : VerbType, new()
     where TNoun : class, new()
 {
-    public TVerb Verb { get; init; }
-    public TNoun Noun { get; set; }
+    [XmlIgnore]
+    public string Name { get; init; }
+    [XmlIgnore]
+    public string Namespace { get; init; }
+    [XmlIgnore]
+    public string Prefix { get; init; }
+    [XmlIgnore]
+    public string? NounName { get; init; }
 
-    public GenericDataAreaType()
+    public GenericDataAreaType<TVerb, TNoun> DataArea { get; set; }
+
+    public GenericBodType()
     {
-        Verb = new TVerb();
-        Noun = new TNoun();
+        // TODO: how to ensure we capture the bod info on Deserialization?
+        Name = "";
+        Namespace = "";
+        Prefix = "";
+        NounName = null;
+        DataArea = new GenericDataAreaType<TVerb, TNoun>();
+    }
+
+    public GenericBodType(string name, string ns, string prefix = "", string? nounName = null)
+    {
+        Name = name;
+        Namespace = ns;
+        Prefix = prefix;
+        NounName = nounName;
+        DataArea = new GenericDataAreaType<TVerb, TNoun>();
+    }
+
+    public XmlSerializer CreateSerializer(Type[]? extraTypes = null)
+    {
+        return new XmlSerializer(this.GetType(), XmlAttributeOverrides, extraTypes, null, Namespace);
+    }
+
+    public XmlAttributeOverrides XmlAttributeOverrides
+    {
+        get
+        {
+            var attrOverrides = new XmlAttributeOverrides();
+            var attributes = new XmlAttributes();
+            attributes.XmlType = new XmlTypeAttribute(Name) { Namespace = Namespace };
+            attrOverrides.Add(this.GetType(), "", attributes );
+
+            attributes = new XmlAttributes();
+            attributes.XmlElements.Add(
+                new XmlElementAttribute("DataArea", DataArea.GetType()) { Namespace = Namespace }
+            );
+            attrOverrides.Add(this.GetType(), "DataArea", attributes );
+
+            // Data Area overrides
+            attributes = new XmlAttributes();
+            attributes.XmlElements.Add(
+                new XmlElementAttribute(typeof(TVerb).Name.Replace("Type", ""), typeof(TVerb)) { Namespace = Oagis.Namespace.URI }
+            );
+            attrOverrides.Add(DataArea.GetType(), "Verb", attributes);
+
+            // Noun overrides
+            attributes = new XmlAttributes();
+            attributes.XmlElements.Add(
+                typeof(TNoun).IsAssignableTo(typeof(System.Collections.IEnumerable)) ?
+                new XmlElementAttribute(NounName ?? Name.Replace(typeof(TVerb).Name.Replace("Type", ""), ""), typeof(TNoun).GenericTypeArguments[0]) { Namespace = Namespace }
+                : new XmlElementAttribute(NounName ?? Name.Replace(typeof(TVerb).Name.Replace("Type", ""), ""), typeof(TNoun)) { Namespace = Namespace }
+            );
+            attrOverrides.Add(DataArea.GetType(), "Noun", attributes);
+
+            return attrOverrides;
+        }
+    }
+
+    public XmlSerializerNamespaces Namespaces
+    {
+        get
+        {
+            return new XmlSerializerNamespaces(new[]
+            {
+                new XmlQualifiedName("oa", Oagis.Namespace.URI),
+                new XmlQualifiedName(Prefix, Namespace),
+            });
+        }
     }
 }
