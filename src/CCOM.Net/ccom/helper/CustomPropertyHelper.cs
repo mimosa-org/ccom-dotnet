67a0a832d0971295cc3f00466328371b25defe65
Sheetal Suresh <Sheetal.Suresh@unisa.edu.au>
2023-07-26 14:51:09 +0930

    RID-814-CRT-WO-1: Custom Property Helper Class

---
+++ src/CCOM.Net/ccom/helper/CustomPropertyHelper.cs
@@ -0,0 +1,58 @@
+using Ccom;
+
+namespace Ccom;
+
+public class CustomPropertyHelper
+{
+    public static Property CreateNumericProperty(string shortName, object value)
+    {
+        var property = new Property
+        {
+            UUID = new Ccom.UUID(autogen: true),
+            ShortName = new[] {
+                new Ccom.TextType() {
+                    Value = shortName
+                    }},
+            ValueContent = new ValueContent
+            {
+                Item = new NumericType
+                {
+                    Value = value.ToString()
+                }
+            }
+        };
+
+        return property;
+    }
+
+    public static Property CreateTextProperty(string shortName, object value)
+    {
+        var property = new Property
+        {
+            UUID = new Ccom.UUID(autogen: true),
+            ShortName = new[] {
+                new Ccom.TextType() {
+                    Value = shortName
+                } },
+            ValueContent = new ValueContent
+            {
+                Item = new TextType
+                {
+                    Value = value.ToString()
+                }
+            }
+        };
+
+        return property;
+    }
+
+    public static string GetTextPropertyValue(object property)
+    {
+        return ((Ccom.TextType) property).Value; 
+    }
+
+    public static string GetNumericPropertyValue(object property)
+    {
+        return ((Ccom.NumericType) property).Value; 
+    }
+}
